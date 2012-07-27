ab041b6596971dfaa99500d60ca0f7275221a8f6
Julia Edholm <dramagirl1012@yahoo.com>
2012-07-26 23:41:55 -0700

    First build
    home screen, sample league page

---
+++ football/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('siteNavigator.views',
+    # Examples:
+
+    (r'^football/$', 'home'),
+    (r'^football/loginScreen', 'loginPage'),
+    (r'^football/hero', 'hero'),
+    (r'^football/LeagueSample', 'LeagueSample'),
+    # url(r'^$', 'football.views.home', name='home'),
+    # url(r'^football/', include('football.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
---
+++ siteNavigator/views.py
@@ -0,0 +1,27 @@
+# Create your views here.
+from django.http import HttpResponse
+from django.template.loader import get_template
+from django.template import Context, RequestContext
+import datetime
+
+def home(request):
+    t = get_template('homeB.html')
+    c = RequestContext(request, {'smiley':'cheese',})
+    return HttpResponse(t.render(c))
+
+def loginPage(request):
+    t = get_template('HomeSample.html')
+    c = RequestContext(request, {'blah':'cheese',})
+    return HttpResponse(t.render(c))
+    #return render_to_response('TestC.html',{'blah':'buh',}, context_instance=RequestContext(request))
+
+def hero(request):
+    t = get_template('heroHome.html')
+    c = RequestContext(request, {'smiley':'cheese',})
+    return HttpResponse(t.render(c))
+
+def LeagueSample(request):
+    t = get_template('SampleLeague.html')
+    c = RequestContext(request, {'smiley':'cheese',})
+    return HttpResponse(t.render(c))
+ 
