575cd046d69ee92b1e9f02932e0cb63e3cade849
Julia Edholm <dramagirl1012@yahoo.com>
2012-07-27 00:39:40 -0700

    Small changes to sample league page

--- football/settings.py
+++ football/settings.py
@@ -107,7 +107,7 @@ ROOT_URLCONF = 'football.urls'
 WSGI_APPLICATION = 'football.wsgi.application'
 
 TEMPLATE_DIRS = (
-    '/Users/juliaedholm/Desktop/Django/football/Templates/',
+    './Templates/',
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
