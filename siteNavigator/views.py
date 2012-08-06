2cb97c6bc4a10a415a55669b992b891583b937ad
Julia Edholm <dramagirl1012@yahoo.com>
2012-08-05 23:12:52 -0700

    making tables with logic

--- siteNavigator/views.py
+++ siteNavigator/views.py
@@ -1,5 +1,6 @@
 # Create your views here.
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 from django.template.loader import get_template
 from django.template import Context, RequestContext
 import datetime
@@ -20,8 +21,16 @@ def loginPage(request):
 
 def LeagueSample(request):
     t = get_template('SampleLeagueA.html')
+<<<<<<< HEAD
     player={'name':'Joe','wins':'2'}
     c = Context({'person':player,})
+=======
+    player1={'name':'A', 'wins':'1', 'didWin':'yes'}
+    player2={'name':'B', 'wins':'2'}
+    player3={'name':'C', 'wins':'3'}
+    player4={'name':'D', 'wins':'4'}
+    c = Context({'team':[player1,player2,player3,player4]})
+>>>>>>> making tables with logic
     return HttpResponse(t.render(c))
  
 def DreamTeam(request):
